b2547c4b33d49045385f36341020e0b76c5b53c0
kayabahri <153840212+kayabahri@users.noreply.github.com>
2024-09-10 09:06:37 +0300

    actormovies

--- src/pages/actor/ActorMovies.js
+++ src/pages/actor/ActorMovies.js
@@ -1,43 +1,77 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import MovieCard from '../../components/MovieCard';
+import Pagination from '../../components/Pagination';
 import { fetchFilteredMovies } from '../../services/movieService';
-import Skeleton from '../../components/Skeleton';
-import ReactLoadingSkeleton from 'react-loading-skeleton';
+import ReusableHeader from '../../components/ReusableHeader';
+import cinemaImage from '../../assets/cinema.jpg';
 
 const ActorMovies = () => {
   const { actorId } = useParams();
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalMovies, setTotalMovies] = useState(0);
 
   useEffect(() => {
     const getMovies = async () => {
-      const data = await fetchFilteredMovies({ with_cast: actorId });
+      setIsLoading(true);
+      const data = await fetchFilteredMovies({ with_cast: actorId }, currentPage);
       setMovies(data.results);
+      setTotalMovies(data.total_results);
       setIsLoading(false);
     };
 
     getMovies();
-  }, [actorId]);
+  }, [actorId, currentPage]);
+
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  // Boş grid hücreleri için placeholder eklemek için:
+  const moviesPerPage = 24; // 6x4 grid
+  const placeholders = [...Array(moviesPerPage - movies.length).keys()]; // Boş grid hücreleri için
 
   return (
-    <div className="container mx-auto p-6">
-      <h1 className="text-3xl font-bold mb-6">Movies with this Actor</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-        {isLoading ? (
-          [...Array(8)].map((_, index) => (
-            <div key={index} className="block bg-white rounded-lg shadow-lg overflow-hidden">
-              <ReactLoadingSkeleton height={288} />
-              <div className="p-4">
-                <ReactLoadingSkeleton width="70%" height={20} />
-                <ReactLoadingSkeleton width="50%" height={20} />
+    <div className="bg-gray-900 text-white font-ubuntu max-w-custom-max mx-auto">
+      <ReusableHeader
+        title="Movies with Actor"
+        breadcrumb="Actor Movies"
+        backgroundImage={cinemaImage}
+      />
+
+      <div className="container mx-auto px-side-padding py-16">
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-4">
+          {isLoading ? (
+            [...Array(moviesPerPage)].map((_, index) => (
+              <div key={index} className="block bg-white rounded-lg shadow-lg overflow-hidden">
+                <div className="h-[332px] bg-gray-700 animate-pulse"></div>
+                <div className="p-4">
+                  <div className="h-4 bg-gray-700 rounded w-3/4 animate-pulse mb-2"></div>
+                  <div className="h-4 bg-gray-700 rounded w-1/2 animate-pulse"></div>
+                </div>
               </div>
-            </div>
-          ))
-        ) : (
-          movies.map((movie) => (
-            <Skeleton key={movie.id} movie={movie} />
-          ))
-        )}
+            ))
+          ) : (
+            <>
+              {movies.map((movie) => (
+                <MovieCard key={movie.id} movie={movie} />
+              ))}
+              {placeholders.map((placeholder) => (
+                <div key={placeholder} className="block bg-transparent rounded-lg"></div> // Boş yerleri dolduran grid hücreleri
+              ))}
+            </>
+          )}
+        </div>
+        <div className="mt-12">
+          <Pagination
+            moviesPerPage={moviesPerPage}
+            totalMovies={totalMovies}
+            currentPage={currentPage}
+            onPageChange={handlePageChange}
+          />
+        </div>
       </div>
     </div>
   );
